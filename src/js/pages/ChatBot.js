eb9ee87a0f4b0ac904c3a0ab5de1a384ab44f021
chetna singh <chetna.singh@scriptbees.com>
2017-10-06 19:28:13 +0530

    adding chat options

--- src/js/pages/ChatBot.js
+++ src/js/pages/ChatBot.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Button} from 'reactstrap'
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import io from 'socket.io-client';
+import uuid from 'node-uuid'
 
 
 
@@ -7,24 +9,90 @@ class ChatBot extends React.Component {
 
     constructor() {
         super();
+        this.state = {
+            data: {
+              messages: []
+            },
+          }
+
+        this.submit = this.submit.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(name, value) {
+        if (name == 'message') {
+            this.setState({
+                message: value
+            });
+        }
+    }
 
 
-        this.submit = this.submit.bind(this)
-      }
-
-
-    submit(){
+    submit(e){
         console.log("hii login here")
+        e.preventDefault()
+        const data = this.state.data
+        if (data.author)
+          this.createMessage()
+        else
+          this.setAuthor()
         
     }
 
+    setAuthor() {
+        const author = 'Guest'
+        if (!author)
+          return
+       
+        const messages = this.state.data.messages
+        this.setState({
+          data: {
+            author,
+            messages
+          }
+        })
+      }
+
+    createMessage() {
+        console.log("hello message")
+        const data = this.state.data
+        const messages = data.messages
+        console.log(this.state.messages)
+       
+        const message_text = this.state.message
+        this.setState({
+          data: {
+            author: data.author,
+            messages
+          }
+        })
+        
+      }
+
     render() {
         console.log(this.props);
-        return <div>
+        const data = this.state.data
+        let form_input
+        if (!data.author) {
+            form_input = (
+            <div>
+                Hi, what is your name?<br />
+                <Input type="text" ref="author" />
+            </div>
+            )
+        } else {
+            form_input = (
+            <div>
+                Hello { data.author }, type a message:<br />
+                <Input name="message" type="text" ref="message" />
+            </div>
+            )
+        }
 
-           
-                <Button color="success" onClick={this.submit}>Send</Button>
-           
+        return <div>
+            <form onSubmit={ this.submit.bind(this) }>
+            { form_input }
+          </form>
         </div>
     }
 }
